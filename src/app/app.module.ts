ee1707cb8b54909a026cbb3108845f2df93ed1a5
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-07-24 23:38:53 +0900

    feat: weather-card component 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AppWeatherCardComponent } from './app-weather-card/app-weather-card.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AppWeatherCardComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/weather-card/weather-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: ['./weather-card.component.less']
+})
+export class WeatherCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
